76584a991670ad71a1aeaa296302fe9f318f01c4
Kahlil Enmond <slope22@hotmail.com>
2020-08-20 19:42:22 -0400

    everything but switch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Welcome from './Components/welcome/welcome';
+import Clock from './Components/clock/clock'
+import Contact from './Components/contact/contact'
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Navigation from "./Components/navigation/Navigation";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+        <Navigation />
+        <Switch>
+
+          <Route path="/welcome/:name" component={Welcome} />
+          <Route  path="/clock" component={Clock} />
+          <Route path="/contact" component={Contact} />
+
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
